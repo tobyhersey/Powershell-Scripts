5ff26ac612ecf8b0354757b6d36b9b2d0368703e
tobyhersey <tobuys@hotmail.com>
2021-01-15 10:22:30 +0000

    including javaruntime

--- jenkins-windows-slave-prereqs.ps1
+++ jenkins-windows-slave-prereqs.ps1
@@ -9,4 +9,4 @@ if (Test-Path($ChocolateyProfile)) {
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco feature enable -n allowGlobalConfirmation
 choco install git -y 
-choco install java 
+choco install javaruntime 
