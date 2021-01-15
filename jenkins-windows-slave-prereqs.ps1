3275c5e417fd2b6c5951353ca0d72e677cc59ddb
tobyhersey <tobuys@hotmail.com>
2021-01-15 08:48:39 +0000

    jenkins-windows-slave-prereqs.ps1

---
+++ jenkins-windows-slave-prereqs.ps1
@@ -0,0 +1,13 @@
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+ 
+ 
+ 
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco feature enable -n allowGlobalConfirmation
+choco install git -y -force 
+choco install java -force
+choco install javaruntime -force
