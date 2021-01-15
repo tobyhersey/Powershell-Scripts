70f168100ee914096b7aee17930bf9c5266b61ba
tobyhersey <tobuys@hotmail.com>
2021-01-15 10:21:32 +0000

    updating to remove force and removing JDK

--- jenkins-windows-slave-prereqs.ps1
+++ jenkins-windows-slave-prereqs.ps1
@@ -8,6 +8,5 @@ if (Test-Path($ChocolateyProfile)) {
  
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco feature enable -n allowGlobalConfirmation
-choco install git -y -force 
-choco install java -force
-choco install javaruntime -force
+choco install git -y 
+choco install java 
